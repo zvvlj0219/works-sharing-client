c226a80b44312ce19205fe38af39f3fd63e82748
zvvlj0219 <s00302448@gmail.com>
2022-11-24 03:58:59 +0900

    commit t0358

--- helpers/formatStringDate.ts
+++ helpers/formatStringDate.ts
@@ -1,16 +1,3 @@
-// 1 Jan
-// 2 Feb
-// 3 Mar
-// 4 Apr
-// 5 May
-// 6 Jun
-// 7 Jul
-// 8 Aug
-// 9 Sep
-// 10 Oct
-// 11 Nov
-// 12 Dec
-
 export const formatStringDate = (createdDate: string): string => {
     const string_month = createdDate.split(' ')[1]
 
